3d226365e03ef55cd3cee92c42a94f8938502d2b
Erickson Fernandes <ericksound10@gmail.com>
2023-01-29 00:13:54 -0300

    Curso Java Alura

---
+++ Variaveis/variaveis.java
@@ -0,0 +1,11 @@
+package Variaveis;
+
+public class variaveis {
+    public static void main(String[] args) {
+        int idade;
+
+        idade = 35;
+
+        System.out.println("Sua idade é " + idade);
+    }
+}

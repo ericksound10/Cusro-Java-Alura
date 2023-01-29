6f99e91e311c5acdcd774b4935a9ecda9d5921d7
Erickson Fernandes <ericksound10@gmail.com>
2023-01-29 01:10:08 -0300

    Variavel Double

---
+++ Variaveis/PontoFlutuante.java
@@ -0,0 +1,10 @@
+package Variaveis;
+
+public class PontoFlutuante {
+    public static void main(String[] args) {
+        double salario;
+        salario = 1000.00;
+
+        System.out.println("Meu Salario é " + salario);    
+    }
+}

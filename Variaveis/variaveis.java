69b54b1fce40f94cee5686054297f10948ed47e9
Erickson Fernandes <ericksound10@gmail.com>
2023-01-29 00:36:30 -0300

    Variavel int com println

--- Variaveis/variaveis.java
+++ Variaveis/variaveis.java
@@ -4,7 +4,7 @@ public class variaveis {
     public static void main(String[] args) {
         int idade;
 
-        idade = 35;
+        idade = 34;
 
         System.out.println("Sua idade é " + idade);
     }
